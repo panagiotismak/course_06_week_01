678e8a987dee7945399ad29379274e8c5d78a8b9
panagiotismak <panagiotis.makropoulos@gmail.com>
2018-04-13 21:26:02 +0300

    added main page with production

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
     resources :cities, except: [:new, :edit] 
     resources :states, except: [:new, :edit]
   end
+
+  get '/ui' => 'ui#index'
+  get '/ui#' => 'ui#index'
+  root "ui#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
