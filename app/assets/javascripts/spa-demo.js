89e28a2a826253d445c26575e8a03d1e2979403f
panagiotismak <panagiotis.makropoulos@gmail.com>
2018-04-17 07:10:10 +0300

    implementing City resource with angular

--- app/assets/javascripts/spa-demo.js
+++ app/assets/javascripts/spa-demo.js
@@ -8,3 +8,7 @@
 //= require spa-demo/app.module
 //= require spa-demo/app.router
 //= require spa-demo/app.constant
+
+//= require spa-demo/cities/cities.module
+//= require spa-demo/cities/cities.service
+//= require spa-demo/cities/cities.controller
---
+++ app/assets/javascripts/spa-demo/cities/cities.controller.js
@@ -0,0 +1,41 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('spa-demo.cities')
+    .controller('spa-demo.cities.CitiesController', CitiesController);
+
+  CitiesController.$inject = ['spa-demo.citied.City'];
+
+  /* @ngInject */
+  function CitiesController(City) {
+    var vm = this;
+    vm.cities;
+    vm.city;
+
+    activate();
+    return;
+    ////////////////
+
+    function activate() {
+      newFoo();
+    }
+
+    function newCity() {
+      vm.city = new City();
+    }
+    function handleError(response) {
+      console.log(response);
+    }
+    function edit(object, index) {
+    }
+    function create() {
+    }
+    function update() {
+    }
+    function remove() {
+    }
+    function removeElement(elements, element) {
+    }
+  }
+})();
