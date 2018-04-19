3a61595ea3d1d35c1e385b8c7bf1ce3a39fba5ee
panagiotismak <panagiotis.makropoulos@gmail.com>
2018-04-19 07:41:54 +0300

    added github cors expression

--- config/application.rb
+++ config/application.rb
@@ -38,7 +38,7 @@ module Course06Week01
 
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
-        origins '*'
+        origins /https:\/\/\w+\.github\.io/
 
         resource '/api/*', 
           :headers => :any, 
